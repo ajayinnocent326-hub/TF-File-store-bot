b58113004391dccf495c45c3c330f1d9ffca046c
ajayinnocent326-hub <ajayinnocent326@gmail.com>
2025-10-14 02:18:32 -0700

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ F_PIC = os.environ.get("FORCE_PIC", "https://files.catbox.moe/ufzpkn.jpg")
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "0")) # auto delete in seconds
 
 
-PORT = os.environ.get("PORT", "8030")
+PORT = os.environ.get("PORT", "40000")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
